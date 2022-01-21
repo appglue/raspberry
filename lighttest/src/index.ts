d3dac810571405ba49450def148c7a1d936f39a5
cdhewi <carl@x365.com>
2022-01-21 18:15:58 -0500

    test

--- lighttest/src/index.ts
+++ lighttest/src/index.ts
@@ -1,8 +1,12 @@
 var Gpio = require('onoff').Gpio,
-    led = new Gpio(4, 'out');
+    led = new Gpio(4, 'out'),
+    led2 = new Gpio(26, 'out');
 var iv = setInterval(function ()
 {
-    led.writeSync(led.readSync() === 0 ? 1 : 0)
+    let val = led.readSync();
+
+    led.writeSync(val === 0 ? 1 : 0)
+    led2.writeSync(val === 0 ? 0 : 1)
 }, 1000);
 // Stop blinking the LED and turn it off after 5 seconds.  
 setTimeout(function ()
