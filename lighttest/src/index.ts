88eadec19470bc9919ae9d12aa09ddb0d6467bbc
cdhewi <carl@x365.com>
2022-01-21 17:43:19 -0500

    test

--- lighttest/src/index.ts
+++ lighttest/src/index.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-const Gpio = require('../onoff').Gpio; // Gpio class
+const Gpio = require('onoff').Gpio; // Gpio class
 const led = new Gpio(17, 'out');       // Export GPIO17 as an output
 
 // Toggle the state of the LED connected to GPIO17 every 200ms
